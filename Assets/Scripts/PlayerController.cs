de0f8fbac68dd5cebef544b22f74810f9718a2d5
DantElemer <farkasdomonkosl@gmail.com>
2017-02-09 14:24:06 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody2D myRb;
+	public float jumpForce;
+
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonDown("Jump"))
+		{
+				myRb.AddForce(transform.up * jumpForce);
+		}
+	}
+}
